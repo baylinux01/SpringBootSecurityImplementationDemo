38f07ed075968847329b0d9fb80cd599235a2bac
baylinux01 <baylinux01@gmail.com>
2024-09-25 20:45:54 +0300

    cors hatası tam manasıyla çözüldü

--- securityImplementationDemo/src/main/java/baylinux01/securityImplementationDemo/configurations/SecurityConfig.java
+++ securityImplementationDemo/src/main/java/baylinux01/securityImplementationDemo/configurations/SecurityConfig.java
@@ -1,5 +1,7 @@
 package baylinux01.securityImplementationDemo.configurations;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,8 +22,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import baylinux01.securityImplementationDemo.filters.JWTFilter;
+import io.jsonwebtoken.lang.Arrays;
 
 
 
@@ -52,6 +57,16 @@ public class SecurityConfig{
 				.httpBasic(Customizer.withDefaults())
 				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.addFilterBefore(jWTFilter, UsernamePasswordAuthenticationFilter.class)
+				.cors(httpSecurityCorsConfigurer->
+				{
+					CorsConfiguration configuration=new CorsConfiguration();
+					configuration.setAllowedOrigins(List.of("*"));
+					configuration.setAllowedMethods(List.of("*"));
+					configuration.setAllowedHeaders(List.of("*"));
+					UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+					source.registerCorsConfiguration("/**", configuration);
+					httpSecurityCorsConfigurer.configurationSource(source);
+				})
 				.build();
 				
 	}
